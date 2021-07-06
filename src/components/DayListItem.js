306fdad1f8c6d115a00eadebc4b0bc1f12b36e6f
Sam Roy-Effer <samroyeffer@gmail.com>
2021-07-06 13:34:07 -0400

    add datlist item

--- src/components/DayListItem.js
+++ src/components/DayListItem.js
@@ -6,13 +6,23 @@ export default function DayListItem(props) {
   const dayClass = classNames("day-list", {
     "day-list__item": true,
     "day-list__item--selected": props.selected === true,
-    "day-list__item--full": props.spot === 0,
+    "day-list__item--full": props.spots === 0,
   });
 
+  const formatSpots = (spots) => {
+    if (spots === 0) {
+      return "no spots remaining";
+    } else if (spots === 1) {
+      return "1 spots remaining";
+    } else if (spots >= 2) {
+      return `${spots} spots remaining`;
+    }
+  };
+
   return (
-    <li className={dayClass} onClick={props.onClick}>
+    <li className={dayClass} onClick={() => props.setDay(props.name)}>
       <h2 className="text--regular">{props.name}</h2>
-      <h3 className="text--light">{props.spots}</h3>
+      <h3 className="text--light">{formatSpots(props.spots)}</h3>
     </li>
   );
 }
