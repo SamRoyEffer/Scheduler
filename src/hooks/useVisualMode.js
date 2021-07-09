c6364d734019d937b5da22c7eb6ba643af017800
Sam Roy-Effer <samroyeffer@gmail.com>
2021-07-09 10:23:37 -0400

    redo useVisual and test file

--- src/hooks/useVisualMode.js
+++ src/hooks/useVisualMode.js
@@ -1,14 +1,10 @@
 import { useState } from "react";
 
 export default function useVisualMode(initial) {
-  const [mode, setMode] = useState(initial);
-  const [history, setHistory] = useState([initial]);
-  function transition() {
-    /* ... */
+  export default function useVisualMode(initial) {
+    const [mode, setMode] = useState(initial);
+  
+    return { mode };
   }
-  function back() {
-    /* ... */
-  }
-
-  return { mode, transition, back };
+  
 }
