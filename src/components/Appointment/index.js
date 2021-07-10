f4395e645e73cebbac8690ae6552a40570d18f13
Sam Roy-Effer <samroyeffer@gmail.com>
2021-07-10 16:07:21 -0400

    implement edit

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -17,6 +17,7 @@ const CREATE = "CREATE";
 const SAVING = "SAVING";
 const DELETING = "DELETING";
 const CONFIRM_DELETE = "CONFIRM_DELETE";
+const EDIT = "EDIT";
 
 export default function Appointment(props) {
   const { mode, transition, back } = useVisualMode(
@@ -54,11 +55,21 @@ export default function Appointment(props) {
         <Show
           student={props.interview.student}
           interviewer={props.interview.interviewer}
+          onEdit={() => transition(EDIT)}
           onDelete={() => transition(CONFIRM_DELETE)}
         />
       )}
       {mode === CREATE && (
         <Form onSave={save} interviewers={props.interviewers} onCancel={back} />
+      )}
+      {mode === EDIT && (
+        <Form
+          name={props.interview.student}
+          interviewer={props.interview.interviewer.id}
+          interviewers={props.interviewers}
+          onSave={save}
+          onCancel={back}
+        />
       )}
       {mode === SAVING && <Status message={"SAVING"} />}
       {mode === CONFIRM_DELETE && (
